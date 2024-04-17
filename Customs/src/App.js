be3f18a4e6cd3836cee8b09f3ade67fdb746b415
VinuHegde17 <95705229+VinuHegde17@users.noreply.github.com>
2024-04-17 10:48:59 +0530

    slightly updated

--- Customs/src/App.js
+++ Customs/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Examples from './components/Examples';
+import HeroSection from './components/Hero';
 import Lemon from './components/Lemon';
+import Navigation from './components/Navigation';
 import ParentComponent from './components/Relement';
 
 function App() {
   return (
     <div className="App">
-      <Examples/>
+      {/* <Examples/>
       <ParentComponent/>
-      <Lemon/>
+      <Lemon/> */}
+      <Navigation/>
+      <HeroSection/>
+
     </div>
   );
 }
